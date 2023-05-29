c7b4f2c4d6a5e91342566e03d247a204b180b9fa
AEGagola <tonygagola@gmail.com>
2023-05-29 13:47:55 -0700

    Add getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+
+    let i = Math.floor(Math.random() * 3);
+    let compChoice = "paper";
+
+    if (i == 0) {
+        compChoice = "rock";
+    } else if (i == 1) {
+        compChoice = "scissor";
+    }
+
+    return compChoice;
+}
+
+console.log(getComputerChoice())
